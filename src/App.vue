949a571d2d42793ba0e4bb00a6e29d16f3038838
hongfuqing <hongfuqing@haitao.com>
2021-04-20 10:27:13 +0800

    项目初始化 项目目录配置

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+	/* 里面有个别名配置vue.config.js里面 */
+	@import url("assets/css/base.css");
+	@import url("assets/css/normalize.css");
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+module.exports={
+	configureWebpack:{
+		resolve:{
+			alias:{
+				'assets':'@/assets',
+				'common':'@/common',
+				'components':'@/comcomponents',
+				'network':'@/network',
+				'views':'@/views'
+			}
+		}
+	}
+}
